547853534f23041cb17761ac9139a5aaf8ed8e3d
GonzaloCarcamo <59894766+GonzaloCarcamo@users.noreply.github.com>
2020-02-23 18:52:00 -0300

    Simple console app with NodeJS

---
+++ calculator.js
@@ -0,0 +1,23 @@
+'use strict'
+
+console.log("Console App with NodeJS");
+
+var params = process.argv.slice(2);
+
+var number1 = parseFloat(params[0]);
+var number2 = parseFloat(params[1]);
+
+//console.log(params);
+//console.log(number1);
+//console.log(number2);
+
+
+var sum = `
+1. the sum is: ${number1 + number2}
+2. the subtraction is: ${number1 - number2}
+3. the multiplication is: ${number1 * number2}
+4. the division is: ${number1 / number2}
+`;
+
+console.log(sum);
+
